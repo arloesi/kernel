c4f1461cd2d249761847bf2e4459c0f4f801ab12
david <ddk128@gmail.com>
2013-09-13 17:03:02 +0100

    Add apache camel.

--- src/main/scala/kernel/handler/Server.scala
+++ src/main/scala/kernel/handler/Server.scala
@@ -17,8 +17,6 @@ import io.netty.bootstrap.ServerBootstrap
 import io.netty.channel.ChannelOption
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.nio.NioServerSocketChannel
-import io.netty.channel.socket.nio.NioServerSocketChannel
-
 
 class Handler extends ChannelInboundHandlerAdapter {
     private val CONTENT =
