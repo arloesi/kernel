0817b246e3c0297fd20713fff5f75f047b247a4b
david <ddk128@gmail.com>
2013-11-07 01:18:32 +0000

    Remove legacy content classes.

--- src/main/scala/kernel/content/Common.java
+++
@@ -1,9 +0,0 @@
-package kernel.content;
-
-import org.eclipse.persistence.queries.FetchGroup;
-
-public class Common {
-  public static void addAttribute(FetchGroup parent, String name, FetchGroup child) {
-    parent.addAttribute(name,child);
-  }
-}
