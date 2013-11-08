4cf2ed0fa8883c896201201acb17846c4ae2a6a4
david <ddk128@gmail.com>
2013-11-08 01:41:21 +0000

    Add Factory

---
+++ src/main/scala/kernel/model/Service.scala
@@ -0,0 +1,21 @@
+package kernel.model
+
+import kernel.network.Socket
+import kernel.service.Remote
+
+class Service {
+    @Remote def subscribe(socket:Socket) {
+    }
+
+    @Remote def unsubscribe(socket:Socket) {
+    }
+
+    @Remote def create(socket:Socket) {
+    }
+
+    @Remote def update(socket:Socket) {
+    }
+
+    @Remote def destroy(socket:Socket) {
+    }
+}
