b24595ae47d09f30af56156277c254fc813f0f4b
David <ddk128@gmail.com>
2013-12-22 20:56:32 +0100

    Add socket test.

---
+++ src/test/scala/kernel/network/SocketTest.scala
@@ -0,0 +1,14 @@
+package kernel.network
+
+import org.junit._
+
+import org.mockito.Mockito._
+import org.mockito.Matchers._
+import org.mockito.BDDMockito._
+
+import org.vertx.java.core._
+import org.vertx.java.core.http._
+
+class SocketTest {
+
+}
